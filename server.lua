c5212368755f1bd0f404db72a7d678bfad19b7d4
No331 <no3fivem@gmail.com>
2025-08-18 00:33:44 +0200

    Corriger la suppression automatique des instances

--- server.lua
+++ server.lua
@@ -24,8 +24,11 @@ end
 -- Fonction pour supprimer une instance
 function deleteInstance(instanceId)
     if instances[instanceId] then
-        print("^1[DUEL] Instance " .. instanceId .. " supprimée^7")
+        local instance = instances[instanceId]
+        print("^1[DUEL] Instance " .. instanceId .. " supprimée (propriétaire: " .. instance.owner .. ", arène: " .. instance.arena .. ")^7")
         instances[instanceId] = nil
+    else
+        print("^1[DUEL] Tentative de suppression d'une instance inexistante: " .. instanceId .. "^7")
     end
 end
 
@@ -53,11 +56,14 @@ AddEventHandler('duel:joinArena', function(weapon, map)
     local currentInstanceId, currentInstance = getPlayerInstance(source)
     if currentInstanceId then
         print("^1[DUEL] Joueur " .. source .. " déjà dans l'instance " .. currentInstanceId .. "^7")
-        return
+        -- Supprimer l'ancienne instance et en créer une nouvelle
+        deleteInstance(currentInstanceId)
     end
     
     -- Créer une nouvelle instance privée
     local instanceId = createInstance(source, map)
+    
+    print("^2[DUEL] Instance " .. instanceId .. " créée avec succès pour " .. playerName .. "^7")
     
     -- Confirmer au client
     TriggerClientEvent('duel:instanceCreated', source, instanceId, weapon, map)
@@ -74,57 +80,77 @@ AddEventHandler('duel:quitArena', function()
     -- Trouver et supprimer l'instance du joueur
     local instanceId, instance = getPlayerInstance(source)
     if instanceId then
+        print("^3[DUEL] Suppression de l'instance " .. instanceId .. " pour le joueur " .. source .. "^7")
         deleteInstance(instanceId)
         TriggerClientEvent('duel:instanceDeleted', source)
+    else
+        print("^1[DUEL] Aucune instance trouvée pour le joueur " .. source .. "^7")
     end
 end)
 
 -- Nettoyer les instances quand un joueur se déconnecte
 AddEventHandler('playerDropped', function(reason)
     local source = source
-    local playerName = GetPlayerName(source)
+    local playerName = GetPlayerName(source) or "Joueur inconnu"
     
     print("^1[DUEL] " .. playerName .. " s'est déconnecté, nettoyage de son instance^7")
     
     local instanceId, instance = getPlayerInstance(source)
     if instanceId then
+        print("^1[DUEL] Suppression de l'instance " .. instanceId .. " suite à déconnexion de " .. playerName .. "^7")
         deleteInstance(instanceId)
+    else
+        print("^3[DUEL] Aucune instance à nettoyer pour " .. playerName .. "^7")
     end
 end)
 
 -- Commande admin pour voir les instances actives
 RegisterCommand('duel_instances', function(source, args, rawCommand)
-    if source == 0 then -- Console serveur seulement
+    if source == 0 or IsPlayerAceAllowed(source, "command.duel_instances") then
         print("^2[DUEL] Instances actives:^7")
         local count = 0
         for instanceId, instance in pairs(instances) do
             count = count + 1
-            local playerName = GetPlayerName(instance.owner)
+            local playerName = GetPlayerName(instance.owner) or "Joueur déconnecté"
+            local timeElapsed = os.time() - instance.created
             print("^3  Instance " .. instanceId .. ": " .. playerName .. " (" .. instance.owner .. ") - Arène: " .. instance.arena .. "^7")
+            print("^3    Créée il y a " .. timeElapsed .. " secondes^7")
         end
         if count == 0 then
             print("^1  Aucune instance active^7")
         end
+        
+        if source ~= 0 then
+            TriggerClientEvent('chat:addMessage', source, {
+                color = {0, 255, 0},
+                multiline = true,
+                args = {"[DUEL]", count .. " instance(s) active(s). Voir console F8 pour détails."}
+            })
+        end
     end
-end, true)
 
 -- Nettoyage automatique des instances anciennes (toutes les 5 minutes)
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(300000) -- 5 minutes
+        Citizen.Wait(1800000) -- 30 minutes
         
         local currentTime = os.time()
         local toDelete = {}
         
         for instanceId, instance in pairs(instances) do
-            -- Supprimer les instances de plus de 30 minutes
-            if currentTime - instance.created > 1800 then
+            -- Supprimer les instances de plus de 2 heures
+            if currentTime - instance.created > 7200 then
                 table.insert(toDelete, instanceId)
             end
         end
         
         for _, instanceId in ipairs(toDelete) do
-            print("^1[DUEL] Instance " .. instanceId .. " supprimée (trop ancienne)^7")
+            print("^1[DUEL] Instance " .. instanceId .. " supprimée automatiquement (trop ancienne - plus de 2h)^7")
+            -- Informer le joueur que son instance a été supprimée
+            local instance = instances[instanceId]
+            if instance and instance.owner then
+                TriggerClientEvent('duel:instanceDeleted', instance.owner)
+            end
             deleteInstance(instanceId)
         end
     end
